ca64d1d6239cba8624654bca7f6f9745923f33a2
dakir8 <thioyq@gmail.com>
2016-03-24 01:22:54 +0800

    changed.

---
+++ app/src/androidTest/java/sample/daniel/ecomsample/SimpleTest.java
@@ -0,0 +1,40 @@
+package sample.daniel.ecomsample;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Daniel on 16/3/24.
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class SimpleTest {
+    private String mStringToBetyped;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        mStringToBetyped = "Hello World!";
+    }
+
+    @Test
+    public void changeText_sameActivity() {
+        onView(withId(R.id.textView))
+                .check(matches(withText(mStringToBetyped)));
+    }
+}
